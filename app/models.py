8d6ada90bb449944a4bf84e4d5e3cf19438844a4
gilangirsyd <gilang.safera.gs@gmail.com>
2025-06-05 05:50:04 +0700

    feat: table review

--- app/models.py
+++ app/models.py
@@ -1,11 +1,23 @@
-from sqlalchemy import Column, String, DECIMAL, Integer, TIMESTAMP, Text
+from sqlalchemy import Column, String, DECIMAL, Integer, TIMESTAMP, Text, ForeignKey, Enum
 from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.sql import func
 from sqlalchemy.orm import declarative_base
 import uuid
+import enum
 
 Base = declarative_base()
 Metadata = Base.metadata
+
+class SentimentEnum(str, enum.Enum):
+    POSITIVE = "POSITIVE"
+    NEGATIVE = "NEGATIVE"
+    NEUTRAL = "NEUTRAL" 
+    
+class CategoryEnum(str, enum.Enum):
+    FEATURE = "FEATURE"
+    BUG = "BUG"
+    UX = "UX"
+    NOISE = "NOISE"
 
 class Application(Base):
     __tablename__ = "applications"
@@ -22,3 +34,20 @@ class Application(Base):
     created_at = Column(TIMESTAMP(timezone=True), server_default=func.now())
     updated_at = Column(TIMESTAMP(timezone=True), onupdate=func.now())
     token_next_scrape = Column(Text)
+    
+class Review(Base):
+    __tablename__ = "reviews"
+    
+    review_id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, nullable=False)
+    application_id = Column(UUID(as_uuid=True)), ForeignKey("applications.application_id", nullable=False)
+    content= Column(Text, nullable=True)
+    username = Column(String(100), nullable=False)
+    score = Column(DECIMAL(3, 2), nullable=False)
+    user_pict = Column(String(255), nullable=True)
+    version_app = Column(String(50), nullable=True)
+    at = Column(TIMESTAMP, nullable=False)
+    reply_content = Column(Text, nullable=True)
+    reply_at = Column(TIMESTAMP, nullable=True)
+    url = Column(String(255), nullable=True) 
+    sentiment = Column(Enum(SentimentEnum, name="sentiment_enum"), nullable=False)
+    category = Column(Enum(CategoryEnum, name="category_enum"), nullable=False)
